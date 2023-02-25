795a45057b7a794849e2a2766d3204ae6b8674c2
Maksim Arkhipov <manjanfe@gmail.com>
2023-02-25 16:31:12 +0700

    Serializers advanced settings.

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,21 +1,52 @@
+import datetime as dt
+# import webcolors
+
 from rest_framework import serializers
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
+
+
+# class Hex2NameColor(serializers.Field):
+#     def to_representation(self, value):
+#         return value
+#
+#     def to_internal_value(self, data):
+#         try:
+#             data = webcolors.hex_to_name(data)
+#         except ValueError:
+#             raise serializers.ValidationError('Для этого цвета нет имени')
+#         return data
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name')
 
 
 class CatSerializer(serializers.ModelSerializer):
     # owner = serializers.StringRelatedField(read_only=True)
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # color = Hex2NameColor()
+    color = serializers.ChoiceField(choices=CHOICES)  # models or serializers
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = (
+            'id',
+            'name',
+            'color',
+            'birth_year',
+            'owner',
+            'achievements',
+            'age',
+        )
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         if 'achievements' not in self.initial_data:
@@ -33,6 +64,21 @@ class CatSerializer(serializers.ModelSerializer):
             )
         return cat
 
+    # def update(self, instance, validated_data):
+    #     if 'achievements' not in self.initial_data:
+    #         return super().update(instance, self.validated_data)
+    #     achievements = validated_data.pop('achievements')
+    #     cat = super().update(instance, self.validated_data)
+    #     for achievement in achievements:
+    #         current_achievement, status = Achievement.objects.get_or_create(
+    #             **achievement
+    #         )
+    #         AchievementCat.objects.create(
+    #             achievement=current_achievement, cat=cat
+    #         )
+    #     cat.achievements = achievements
+    #     return cat
+
 
 class OwnerSerializer(serializers.ModelSerializer):
     cats = serializers.StringRelatedField(many=True, read_only=True)
@@ -40,3 +86,6 @@ class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
+
+
+
